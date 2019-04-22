a00b314081367b12ed8a57edae2929c6b77f196a
slukas23 <lukasschorn23@gmail.com>
2019-04-22 18:33:05 +0200

    welcome page nearly ready

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,30 @@
-const express = require('express');
-const router = express.Router();
+const express = require("express")
+const router = express.Router()
 
 /* GET home page */
 // router.get('/login', (req, res, next) => {
 //   res.render('auth/login');
 // });
 
-router.get('/welcome', (req, res, next) => {
-  res.render('userarea/welcome');
-});
+router.get("/welcome", (req, res, next) => {
+    console.log(req.user)
+    res.render("userarea/welcome", { username: req.user.username })
+})
 
-router.get('/create-plan', (req, res, next) => {
-  res.render('userarea/create-plan');
-});
+router.get("/create-plan", (req, res, next) => {
+    res.render("userarea/create-plan")
+})
 
-router.get('/insert-meals', (req, res, next) => {
-  res.render('userarea/insert-meals');
-});
+router.get("/insert-meals", (req, res, next) => {
+    res.render("userarea/insert-meals")
+})
 
-router.get('/add-meals', (req, res, next) => {
-  res.render('userarea/add-meals');
-});
+router.get("/add-meals", (req, res, next) => {
+    res.render("userarea/add-meals")
+})
 
-router.get('/shoppinglist', (req, res, next) => {
-  res.render('userarea/shoppinglist');
-});
+router.get("/shoppinglist", (req, res, next) => {
+    res.render("userarea/shoppinglist")
+})
 
-module.exports = router;
+module.exports = router
