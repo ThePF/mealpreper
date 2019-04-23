227257d45daa61297efa03318f40143b501d2600
slukas23 <lukasschorn23@gmail.com>
2019-04-23 12:09:50 +0200

    added landing page

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const router = express.Router();
+const express = require("express")
+const router = express.Router()
 // const planSchema = require('Plan');
 
 /* GET home page */
@@ -7,31 +7,39 @@ const router = express.Router();
 //   res.render('auth/login');
 // });
 
+//CODE BELOW ADDED BY LUKAS TUESDAY MORNING
+
+router.get("/", (req, res, next) => {
+    res.render("index")
+})
+
+// ------ UNTIL HERE ----------------
+
 const authenticationCheck = (req, res, next) => {
-  if (req.isAuthenticated()) next();
-  else res.redirect('/auth/login');
-};
+    if (req.isAuthenticated()) next()
+    else res.redirect("/auth/login")
+}
 
-router.get('/welcome', authenticationCheck, (req, res, next) => {
-  console.log(req.user);
-  res.render('userarea/welcome', { username: req.user.username });
-});
+router.get("/welcome", authenticationCheck, (req, res, next) => {
+    console.log(req.user)
+    res.render("userarea/welcome", { username: req.user.username })
+})
 
-router.get('/create-plan', authenticationCheck, (req, res, next) => {
-  let weekArray = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-  res.render('userarea/create-plan', { weekArray });
-});
+router.get("/create-plan", authenticationCheck, (req, res, next) => {
+    let weekArray = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    res.render("userarea/create-plan", { weekArray })
+})
 
-router.get('/insert-meals', (req, res, next) => {
-  res.render('userarea/insert-meals');
-});
+router.get("/insert-meals", (req, res, next) => {
+    res.render("userarea/insert-meals")
+})
 
-router.get('/add-meals', (req, res, next) => {
-  res.render('userarea/add-meals');
-});
+router.get("/add-meals", (req, res, next) => {
+    res.render("userarea/add-meals")
+})
 
-router.get('/shoppinglist', (req, res, next) => {
-  res.render('userarea/shoppinglist');
-});
+router.get("/shoppinglist", (req, res, next) => {
+    res.render("userarea/shoppinglist")
+})
 
-module.exports = router;
+module.exports = router
